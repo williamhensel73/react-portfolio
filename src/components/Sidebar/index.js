edb0d170cca6428fd1b1cbab8d258f2488153e06
WilliamHensel <124915195+WilliamHensel@users.noreply.github.com>
2024-06-12 17:24:57 -0500

    6/12/24 gonna take break. I want a different font for the body, other than La Belle (layout/index.scss). also weird issue with the font for the buttons on nav-bar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,41 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoW from '../../assets/images/HeadShot.JPG'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+//create a image to go under the headshot
+//it can simply say my name.
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className= "logo" to="/">
+            <img src={LogoW} alt="Headshot of Will Hensel" />
+            <img className="sub-logo" src={LogoSubtitle} alt="William Hensel in text" />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#FFFFFF"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#FFFFFF"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#FFFFFF"/>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank"
+                 rel="nonreferrer"
+                href="https://www.linkedin.com/in/william-hensel-940211250/"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#FFFFFF"/>
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
