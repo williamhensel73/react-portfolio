726e607151a6d8ee1f256a7b1a82772a3c9a0089
WilliamHensel <124915195+WilliamHensel@users.noreply.github.com>
2024-07-10 19:33:05 -0500

    added a portfolio page, added full responsivness. The portfolio page doesnt have any actual projects but could be used in firebase. One responsive improvements could be on the home page with the E in Engineer. Otherwise it could benifit from more details in design and clean up.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,26 +3,37 @@ import './index.scss'
 import LogoW from '../../assets/images/HeadShot.JPG'
 import LogoSubtitle from '../../assets/images/HenselTextWhite.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
 
-const Sidebar = () => (
+const Sidebar = () => {
+
+    const [showNav, setShowNav] = useState(false);
+
+    return (
     <div className="nav-bar">
         <Link className= "logo" to="/">
             <img src={LogoW} alt="Headshot of Will Hensel" />
             <img className="sub-logo" src={LogoSubtitle} alt="William Hensel in text" />
         </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+        <nav className={showNav ? 'mobile-show' : ''}>
+            <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color="#FFFFFF"/>
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#FFFFFF"/>
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                <FontAwesomeIcon icon={faSuitcase} color="#FFFFFF"/>
+            </NavLink>
+            <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#FFFFFF"/>
             </NavLink>
+
+            <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color='#ffd700' size='3x' className='closeIcon'/>
+
         </nav>
         <ul>
             <li>
@@ -34,7 +45,13 @@ const Sidebar = () => (
                 </a>
             </li>
         </ul>
+        <FontAwesomeIcon 
+        onClick={() => setShowNav(true)}
+        icon={faBars} 
+        color='#ffd700' 
+        size='3x' 
+        className='hamburger-icon'/>
     </div>
-)
-
+    )
+}
 export default Sidebar
