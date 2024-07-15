9472f6084837ff8cc2f5ec80e08bf6308176efe2
WilliamHensel <124915195+WilliamHensel@users.noreply.github.com>
2024-07-15 17:11:47 -0500

    browserrouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.scss'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Portfolio from './components/Portfolio'
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/portfolio" element={<Portfolio/>}/>
-      </Route>
-    </Routes>
+    <Router basename="/react-portfolio">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="portfolio" element={<Portfolio />} />
+        </Route>
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
