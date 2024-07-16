b8a5f62024e30ccff8df9f5aebe34b61aa5e08be
WilliamHensel <124915195+WilliamHensel@users.noreply.github.com>
2024-07-15 19:42:45 -0500

    bug finally found because of browser router in index.js and gh pages doesnt like that router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
@@ -8,16 +8,14 @@ import Portfolio from './components/Portfolio'
 
 function App() {
   return (
-    <HashRouter basename='/react-portfolio'>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home/>}/>
         <Route path="about" element={<About/>}/>
-        <Route path="contact" element={<Contact/>}/>
-        <Route path="portfolio" element={<Portfolio/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/portfolio" element={<Portfolio/>}/>
       </Route>
     </Routes>
-    </HashRouter>
   )
 }
 
